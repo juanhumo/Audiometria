f5599ff5454d65839f2da6596e14249c540929ce
Juan Humo <juanma_bio82@hotmail.com>
2023-03-03 22:53:28 -0300

    se añadió la selección del tipo de marcadores

--- funciones.js
+++ funciones.js
@@ -11,7 +11,6 @@ function drawGrid() {
         ctx.moveTo(x, 0);
         ctx.lineTo(x, mainCanvas.clientHeight);
     }
-
     for (let y = 1; y < mainCanvas.clientHeight; y += (mainCanvas.clientHeight) / 15) {
         ctx.moveTo(0, y);
         ctx.lineTo(mainCanvas.clientWidth, y);
@@ -24,18 +23,64 @@ function drawGrid() {
 
 //************************ Grafica los marcadores al hacer clic ***********************//
 const markers = [];
+let simbolo = 8;
 
 const drawMarkers = (evt) => {
 
     if (isSelecting === false) {
         cursorX = evt.offsetX;
         cursorY = evt.offsetY;                
-        ctx.lineWidth = 3;
-        ctx.strokeStyle = "red"; 
-        let triangle = new Triangle(cursorX, cursorY, ctx);
-        markers.push(triangle);               //Almaceno cc del marcador 
-        triangle.draw();   
-      
+        ctx.lineWidth = 3; 
+        
+        if(document.getElementById("circle-marker").checked){
+                let circle = new Circle(cursorX, cursorY, ctx);
+                markers.push(circle);
+                circle.draw();
+                
+        }
+        if(document.getElementById("minor-marker").checked){
+                let minor = new Minor(cursorX, cursorY, ctx);
+                markers.push(minor);
+                minor.draw();
+                
+        }
+        if(document.getElementById("triangle-marker").checked){
+                let triangle = new Triangle(cursorX, cursorY, ctx);
+                markers.push(triangle);               //Almaceno cc del marcador 
+                triangle.draw();   
+                
+        }
+        if(document.getElementById("bracket-open-marker").checked){    
+                let bracketopen = new SquareBracketOpen(cursorX, cursorY, ctx);
+                markers.push(bracketopen);
+                bracketopen.draw();
+                
+        }
+        if(document.getElementById("x-marker").checked){
+                let ex = new Ex(cursorX, cursorY, ctx);
+                markers.push(ex);
+                ex.draw();
+                
+        }
+        if(document.getElementById("major-marker").checked){
+                let major = new Major(cursorX, cursorY, ctx);
+                markers.push(major);
+                major.draw();
+                
+        }
+        if(document.getElementById("square-marker").checked){
+                let square = new Square(cursorX, cursorY, ctx);
+                markers.push(square);
+                square.draw();
+                
+        }
+        if(document.getElementById("bracket-close-marker").checked){
+                let bracketclose = new SquareBracketClose(cursorX, cursorY, ctx);
+                markers.push(bracketclose);
+                bracketclose.draw();
+                
+        }      
+        
        console.log(markers);                     
     }
 };
@@ -49,12 +94,10 @@ let startX, startY, endX, endY;
 let isSelecting = false;
 
 const startSelection = (evt) => {      //Captura cc: x-y cuando comienza la selección
-   
-    if(evt.shiftKey===true){
+       
         startX = evt.offsetX;
         startY = evt.offsetY;
-        isSelecting = true;
-    } 
+        isSelecting = true; 
 };
 
 
@@ -79,6 +122,7 @@ const updateSelection = (evt) => { // Genera el cuadro de selección y elimina c
 const endSelection = () => {        // Finaliza la selección, borra y restaura grilla y marcadores
     if (isSelecting === true) {
         ctx.clearRect(0, 0, mainCanvas.clientWidth, mainCanvas.clientHeight); //borra todo
+        ctx.setLineDash([]);                //linea sólida en caso de que este seteada en dashed
         drawGrid();                                                           // Restaura grilla
         ctx.lineWidth = 3;
         ctx.strokeStyle = "red";
@@ -100,11 +144,12 @@ const drawLines = (evt) => {
         function compareX(a, b) { return a.cursorX - b.cursorX; } //Función para sort()
         markers.sort(compareX);                     //ordeno markers de < a > segun ccX  
         ctx.lineWidth = 2;
-
+        
         for (let i = 0; i < markers.length - 1; i++) {
+            ctx.setLineDash([6, 3]);                    // linea interrumpida
             ctx.beginPath();
-            ctx.moveTo(markers[i].cursorX+3, markers[i].cursorY);
-            ctx.lineTo(markers[i + 1].cursorX-3, markers[i + 1].cursorY);
+            ctx.moveTo(markers[i].cursorX, markers[i].cursorY);
+            ctx.lineTo(markers[i + 1].cursorX, markers[i + 1].cursorY);
             ctx.stroke();
         }
         ctx.closePath();
@@ -115,20 +160,22 @@ const drawLines = (evt) => {
 
 //*************************** Dibujo de distintos marcadores **************************//
 
-function drawCircle() {
-    const circle = new Path2D();
-    circle.arc(100, 35, 25, 0, 2 * Math.PI);
-    ctx.stroke(circle);
+class Circle {
+    constructor(cursorX, cursorY, ctx) {
+        this.cursorX = cursorX;
+        this.cursorY = cursorY;
+        this.ctx = ctx;
+      }
+    draw() {
+        this.ctx.strokeStyle = "red";
+        this.ctx.fillStyle = 'white' 
+        const circle = new Path2D();
+        circle.arc(this.cursorX, this.cursorY, 7, 0, 2 * Math.PI);
+        this.ctx.stroke(circle);
+    }
 }
 
-function drawX(){
-    const eX = new Path2D();
-    eX.moveTo(0, 0);
-    eX.lineTo(20, 20);
-    eX.moveTo(20, 0);
-    eX.lineTo(0, 20);
-    ctx.stroke(eX);
-}
+
 
 class Triangle {
     constructor(cursorX, cursorY, ctx) {
@@ -137,6 +184,7 @@ class Triangle {
         this.ctx = ctx;
       }
     draw() {
+        this.ctx.strokeStyle = "red";
         this.ctx.beginPath();
         this.ctx.moveTo(this.cursorX, this.cursorY-7);
         this.ctx.lineTo(this.cursorX-7, this.cursorY+7);
@@ -147,12 +195,104 @@ class Triangle {
       }
 }
 
+class Square {
+    constructor(cursorX, cursorY, ctx) {
+        this.cursorX = cursorX;
+        this.cursorY = cursorY;
+        this.ctx = ctx;
+      }
+    draw() {
+        this.ctx.strokeStyle = "blue"; 
+        const square = new Path2D();
+        square.rect(this.cursorX-7, this.cursorY-7, 14, 14);
+        this.ctx.stroke(square);
+    }
+}
 
-function drawSimbol(selectedSimbol){
-    
-    const scuare = new Path2D();
-    const minor = new Path2D();
-    const mayor = new Path2D();  
+class Ex {
+    constructor(cursorX, cursorY, ctx) {
+        this.cursorX = cursorX;
+        this.cursorY = cursorY;
+        this.ctx = ctx;
+      }
+    draw() {
+        this.ctx.strokeStyle = "blue"; 
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.cursorX-7, this.cursorY+7);
+        this.ctx.lineTo(this.cursorX+7, this.cursorY-7);
+        this.ctx.moveTo(this.cursorX-7, this.cursorY-7);
+        this.ctx.lineTo(this.cursorX+7, this.cursorY+7);
+        this.ctx.closePath();
+        this.ctx.stroke();
+    }
+}
+
+
+class Minor {
+    constructor(cursorX, cursorY, ctx) {
+        this.cursorX = cursorX;
+        this.cursorY = cursorY;
+        this.ctx = ctx;
+      }
+    draw() {
+        this.ctx.strokeStyle = "red"; 
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.cursorX-2, this.cursorY+7);
+        this.ctx.lineTo(this.cursorX-7, this.cursorY);
+        this.ctx.lineTo(this.cursorX-2, this.cursorY-7);
+        this.ctx.stroke();
+    }
+}
+
+class Major {
+    constructor(cursorX, cursorY, ctx) {
+        this.cursorX = cursorX;
+        this.cursorY = cursorY;
+        this.ctx = ctx;
+      }
+    draw() {
+        this.ctx.strokeStyle = "blue"; 
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.cursorX+2, this.cursorY+7);
+        this.ctx.lineTo(this.cursorX+7, this.cursorY);
+        this.ctx.lineTo(this.cursorX+2, this.cursorY-7);
+        this.ctx.stroke();
+    }
+}
+
+class SquareBracketOpen {
+    constructor(cursorX, cursorY, ctx) {
+        this.cursorX = cursorX;
+        this.cursorY = cursorY;
+        this.ctx = ctx;
+      }
+    draw() {
+        this.ctx.strokeStyle = "roja"; 
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.cursorX-2, this.cursorY+7);
+        this.ctx.lineTo(this.cursorX-7, this.cursorY+7);
+        this.ctx.lineTo(this.cursorX-7, this.cursorY-7);
+        this.ctx.lineTo(this.cursorX-2, this.cursorY-7);
+        this.ctx.stroke();
+    }
+}
+
+
+class SquareBracketClose {
+    constructor(cursorX, cursorY, ctx) {
+        this.cursorX = cursorX;
+        this.cursorY = cursorY;
+        this.ctx = ctx;
+      }
+    draw() {
+        this.ctx.strokeStyle = "blue"; 
+        this.ctx.beginPath();
+        this.ctx.moveTo(this.cursorX+2, this.cursorY+7);
+        this.ctx.lineTo(this.cursorX+7, this.cursorY+7);
+        this.ctx.lineTo(this.cursorX+7, this.cursorY-7);
+        this.ctx.lineTo(this.cursorX+2, this.cursorY-7);
+        this.ctx.stroke();
+    }
 }
 
 
